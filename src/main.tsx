276225b0e414e936f9c999eaf5da20001055e6b8
Yavor Dobrev <yavordobrev24@gmail.com>
2024-06-24 18:11:13 +0300

    Redux store initial setup with Provider and ChakraProvider for UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Provider } from 'react-redux'
+import { store } from './state/store.ts'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </Provider>
 )
