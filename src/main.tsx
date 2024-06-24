22e2525af6898dc85129a5f076eeb92cafc77b9c
Yavor Dobrev <yavordobrev24@gmail.com>
2024-06-24 20:04:41 +0300

    Add BrowserRouter, include Header, and map routes to components.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Provider } from 'react-redux'
-import { store } from './state/store.ts'
+import store from './state/store.ts'
 import App from './App.tsx'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </Provider>
+  </BrowserRouter>
 )
