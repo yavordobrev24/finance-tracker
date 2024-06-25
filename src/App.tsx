ea48c3ea77f9c7429727d6c1ecf44fd5d65f4053
Yavor Dobrev <yavordobrev24@gmail.com>
2024-06-25 18:12:38 +0300

    Wrap a container around Routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,22 @@ import Summary from './components/Summary/Summary'
 import TransactionDetails from './components/TransactionDetails/TransactionDetails'
 import TransactionList from './components/TransactionList/TransactionList'
 import TransactionForm from './components/TransactionForm/TransactionForm'
+import { Container } from '@chakra-ui/react'
 
 function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route index element={<Summary />} />
-        <Route path="transactions">
-          <Route index element={<TransactionList />} />
-          <Route path=":id" element={<TransactionDetails />} />
-          <Route path="create" element={<TransactionForm />} />
-        </Route>
-      </Routes>
+      <Container maxW="1200px" py="30px">
+        <Routes>
+          <Route index element={<Summary />} />
+          <Route path="transactions">
+            <Route index element={<TransactionList />} />
+            <Route path=":id" element={<TransactionDetails />} />
+            <Route path="create" element={<TransactionForm />} />
+          </Route>
+        </Routes>
+      </Container>
     </>
   )
 }
