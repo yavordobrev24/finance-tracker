0cd8e31c6afeba3a3e70e8b2af3a79deafe8d8da
Yavor Dobrev <yavordobrev24@gmail.com>
2024-06-26 16:49:25 +0300

    Add category to initialState and create changeCategory reducer and action

--- src/state/transactions/transactionsSlice.ts
+++ src/state/transactions/transactionsSlice.ts
@@ -1,12 +1,33 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { Transaction } from '../../types'
 
-const initialState: Transaction[] = []
+type TransactionsState = {
+  transactions: Transaction[]
+  filteredTransactions: Transaction[]
+  category: string
+}
 
+const initialState: TransactionsState = {
+  transactions: [],
+  filteredTransactions: [],
+  category: 'All',
+}
 const transactionsSlice = createSlice({
   name: 'transactions',
   initialState,
-  reducers: {},
-})
+  reducers: {
+    changeCategory: (state, action) => {
+      state.category = action.payload
+      if (action.payload == 'All') {
+        state.filteredTransactions = state.transactions
+      } else {
+        state.filteredTransactions = state.transactions.filter(
+          (transaction) => transaction.category == action.payload
+        )
+      }
+    },
+  },
 
+})
+export const { changeCategory } = transactionsSlice.actions
 export default transactionsSlice.reducer
