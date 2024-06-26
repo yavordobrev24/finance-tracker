214676c56c74b47f46a823c9c988c0f0a108e343
Yavor Dobrev <yavordobrev24@gmail.com>
2024-06-26 16:45:46 +0300

    Update setup.ts

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,10 @@
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom/vitest'
+global.matchMedia =
+  global.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    }
+  }
