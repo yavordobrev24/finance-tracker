549d5405caca643d73155f9ef16bde76ad0f4799
Yavor Dobrev <yavordobrev24@gmail.com>
2024-06-26 21:22:40 +0300

    Add test for App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import store from './state/store'
 
-it('should have Vite test', () => {
-  render(<App />)
-  const message = screen.queryByText(/Vite/i)
-  expect(message).toBeVisible()
+describe('App', () => {
+  it('should render App', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    )
+    screen.debug()
+  })
 })
